532ec189835cf20b45d940fed6e14526794df0e1
James Spargo <solacedforever@gmail.com>
2017-07-20 13:32:02 -0400

    changes

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// The Form Data
 let formData = [
-  // For demonstration purposes,
-  // the first form element has been added to the HTML file as a comment
-  // compare the input in the HTML file with the contents of this first object
   {
     "type": "text",
     "label": "First Name",
@@ -81,71 +77,42 @@ let formData = [
     "options": []
   }
 ];
-
-// HINTS:
-// As you can see, we access the first element in the array
-// with [0] and then grab the property "label" using the "." operator
 ( function(){
-  // Select the first element from the array
   let first = formData[ 0 ];
-  // Log the first object
   console.log( first );
-  // Log the string "First Name"
   console.log( first.label );
 } )();
-
-
-// -------- Your Code Goes Below this Line --------
-
 let fields = document.querySelector('#fields');
-
 for (let i = 0; i < formData.length; i++) {
-  if (formData[i].label != "Select Language") {
+    if (formData[i].label != "Select Language") {
     if (formData[i].type != "textarea") {
       let input = document.createElement('input');
-
-     input.type = formData[i].type;
+      input.type = formData[i].type;
       input.placeholder = formData[i].label;
       input.id = formData[i].id;
       input.icon = formData[i].icon;
       input.options = formData[i].options;
-
-     fields.appendChild(input);
-
+      fields.appendChild(input);
    }
   }
-
  if (formData[i].options.length > 0) {
     let dropdown = document.createElement("select");
     let selectOption = `${formData[i].label}`
    let option = document.createElement("option");
-
-   option.setAttribute("selected", "true");
-    option.setAttribute("disabled", "true");
     option.innerHTML = `${selectOption}`;
-
-   dropdown.appendChild(option);
-
-   for (a = 0; a < formData[i].options.length; a++) {
-      let dropdownOption = document.createElement("option");
-
-     dropdownOption.setAttribute("value", formData[i].options[a].value);
-      dropdownOption.innerHTML = formData[i].options[a].label;
-      dropdown.appendChild(dropdownOption);
-
-     fields.appendChild(dropdown);
-
+    dropdown.appendChild(option);
+      for (a = 0; a < formData[i].options.length; a++) {
+          let dropdownOption = document.createElement("option");
+          dropdownOption.setAttribute("value", formData[i].options[a].value);
+          dropdownOption.innerHTML = formData[i].options[a].label;
+          dropdown.appendChild(dropdownOption);
+          fields.appendChild(dropdown);
    }
   }
-
  if (formData[i].type === "textarea") {
     let textarea = document.createElement("textarea");
-
-   textarea.setAttribute("placeholder", `${formData[i].label}`);
-
-   fields.appendChild(textarea);
-
+    textarea.setAttribute("placeholder", `${formData[i].label}`);
+    fields.appendChild(textarea);
  }
 }
-
 console.log(fields);
